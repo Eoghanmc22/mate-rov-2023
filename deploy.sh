73d6205fd54348fd0684fad499951a45f0ad038d
Eoghanmc22 <eoghanmc@icloud.com>
2022-11-28 17:25:08 +0000

    Improve uploader

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cargo run --bin robot --release --target aarch64-unknown-linux-gnu
--- runner-rpi/src/main.rs
+++ runner-rpi/src/main.rs
@@ -1,7 +1,4 @@
-use std::{
-    env,
-    process::{Command, Stdio},
-};
+use std::{env, process::Command};
 
 use anyhow::{bail, Context};
 
@@ -10,47 +7,55 @@ pub fn main() -> anyhow::Result<()> {
         bail!("No binary provided");
     };
 
-    println!("Finding ip");
-    let output = Command::new("doas")
-        .stdin(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .arg("arp-scan")
-        .arg("--plain")
-        .arg("--quiet")
-        .arg("-B")
-        .arg("80M")
-        .arg("-I")
-        .arg(include_str!("../../interface").trim())
-        .arg("169.254.0.0/16")
-        .output()
-        .context("Execute arp-scan")?;
+    eprintln!("Killing process");
 
-    if !output.status.success() {
-        bail!("arp-scan was not successful");
-    }
-
-    let output = core::str::from_utf8(&output.stdout).context("Convert output to utf8")?;
-
-    let Some(ip) = output.split(char::is_whitespace).next() else {
-        bail!("No ip was found");
-    };
-
-    if ip.is_empty() {
-        bail!("Ip was blank");
-    }
-
-    println!("Using ip {ip}");
-
-    Command::new("sshpass")
-        .arg("-p")
-        .arg("raspberry")
-        .arg("scp")
-        .arg(bin)
-        .arg(format!("pi@{ip}:~/mate/exec"))
+    let status = Command::new("ssh")
+        .arg("pi@mate.local")
+        .arg("pkill exec || exit 0")
         .spawn()
-        .context("Execute scp")?;
+        .context("Spawn ssh")?
+        .wait()
+        .context("Wait on ssh")?;
 
-    println!("Success!");
+    if status.success() {
+        eprintln!("Killed");
+    } else {
+        bail!("Could not kill process");
+    }
+    eprintln!();
+
+    eprintln!("Uploading");
+
+    let status = Command::new("scp")
+        .arg(bin)
+        .arg("pi@mate.local:~/mate/exec")
+        .spawn()
+        .context("Spawn scp")?
+        .wait()
+        .context("Wait on scp")?;
+
+    if status.success() {
+        eprintln!("Upload success!");
+    } else {
+        bail!("Upload failed: {status}")
+    }
+    eprintln!();
+
+    eprintln!("Running binary");
+
+    let status = Command::new("ssh")
+        .arg("pi@mate.local")
+        .arg("sudo ~/mate/exec")
+        .spawn()
+        .context("Spawn ssh")?
+        .wait()
+        .context("Wait on ssh")?;
+
+    if status.success() {
+        eprintln!("Remote run success!");
+    } else {
+        bail!("Remote run failed: {status}")
+    }
 
     Ok(())
 }
