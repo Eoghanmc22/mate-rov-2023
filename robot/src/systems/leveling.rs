d96c92d421c05b974c6697849549ff386351c9c4
Eoghanmc22 <eoghanmc@icloud.com>
2023-04-15 18:59:02 -0400

    Implement pid controller

--- robot/src/systems/leveling.rs
+++ robot/src/systems/leveling.rs
@@ -1,11 +1,12 @@
 use std::{
+    sync::Arc,
     thread::{self, Scope},
     time::{Duration, Instant},
 };
 
 use common::{
     error::LogErrorExt,
-    store::{self, tokens},
+    store::{self, tokens, Store, Update},
     types::Orientation,
 };
 use crossbeam::channel::bounded;
@@ -33,13 +34,9 @@ impl System for LevelingSystem {
 
                 for event in listner {
                     match &*event {
-                        Event::Store(update) => {
-                            if let Some(orientation) =
-                                store::handle_update(&tokens::ORIENTATION, update)
-                            {
-                                tx.try_send(LevelingEvent::Orientation(*orientation))
-                                    .log_error("Repeat orientation update");
-                            }
+                        Event::Store(_) | Event::SyncStore | Event::ResetForignStore => {
+                            tx.try_send(LevelingEvent::Event(event))
+                                .log_error("Send Exit");
                         }
                         Event::Exit => {
                             tx.try_send(LevelingEvent::Exit).log_error("Send Exit");
@@ -79,11 +76,30 @@ impl System for LevelingSystem {
             spawner.spawn(move || {
                 span!(Level::INFO, "Leveling fusion thread");
 
-                let mut orientation = Orientation::default();
+                let mut store = {
+                    let mut events = events.clone();
+                    Store::new(move |update| {
+                        events.send(Event::Store(update));
+                    })
+                };
+
+                let mut pitch_controller = PidController::default();
+                let mut roll_controller = PidController::default();
 
                 for event in rx {
                     match event {
-                        LevelingEvent::Orientation(frame) => orientation = frame,
+                        LevelingEvent::Event(event) => match &*event {
+                            Event::SyncStore => {
+                                store.refresh();
+                            }
+                            Event::ResetForignStore => {
+                                store.reset_shared();
+                            }
+                            Event::Store(update) => {
+                                store.handle_update_shared(update);
+                            }
+                            _ => unreachable!(),
+                        },
                         LevelingEvent::Tick => {
                             todo!()
                         }
@@ -100,7 +116,69 @@ impl System for LevelingSystem {
 }
 
 enum LevelingEvent {
-    Orientation(Orientation),
+    Event(Arc<Event>),
     Tick,
     Exit,
+}
+
+#[derive(Default, Clone, Copy)]
+struct PidController {
+    last_error: Option<f32>,
+    integral: f32,
+}
+
+#[derive(Clone, Copy)]
+struct PidConfig {
+    k_p: f32,
+    k_i: f32,
+    k_d: f32,
+
+    max_integral: f32,
+
+    clamp_p: f32,
+    clamp_i: f32,
+    clamp_d: f32,
+
+    clamp_total: f32,
+}
+
+impl PidController {
+    pub fn update(&mut self, error: f32, config: PidConfig) -> f32 {
+        let p = error;
+
+        self.integral = clamp(self.integral + error, config.max_integral);
+        let i = self.integral;
+
+        let d = if let Some(last_error) = self.last_error {
+            error - last_error
+        } else {
+            0.0
+        };
+        self.last_error = Some(error);
+
+        let p = clamp(p * config.k_p, config.clamp_p);
+        let i = clamp(i * config.k_i, config.clamp_i);
+        let d = clamp(d * config.k_d, config.clamp_d);
+
+        clamp(p + i + d, config.clamp_total)
+    }
+}
+
+impl Default for PidConfig {
+    fn default() -> Self {
+        Self {
+            k_p: 0.05,
+            k_i: 0.0,
+            k_d: 0.0,
+            max_integral: 2.0,
+            clamp_p: 0.3,
+            clamp_i: 0.3,
+            clamp_d: 0.3,
+            clamp_total: 0.2,
+        }
+    }
+}
+
+fn clamp(val: f32, range: f32) -> f32 {
+    val.clamp(-range, range)
 }
