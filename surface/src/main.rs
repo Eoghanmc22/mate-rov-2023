ee0c3dee10662ca2ddd4422501fc488772b6151f
Eoghanmc22 <eoghanmc@icloud.com>
2023-05-30 12:12:48 -0400

    Log frame rate

--- surface/src/main.rs
+++ surface/src/main.rs
@@ -4,12 +4,18 @@
 mod plugins;
 
 use crate::plugins::MatePlugins;
-use bevy::prelude::*;
+use bevy::{
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    prelude::*,
+    winit::WinitSettings,
+};
 
 fn main() {
     App::new()
         .insert_resource(FixedTime::new_from_secs(1.0 / 100.0))
         .add_plugins(DefaultPlugins)
         .add_plugins(MatePlugins)
+        .add_plugin(LogDiagnosticsPlugin::default())
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .run();
 }
