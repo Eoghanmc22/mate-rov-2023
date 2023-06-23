308e7bbd97575b36400b24c00748616147f0640a
Eoghanmc22 <eoghanmc@icloud.com>
2023-06-23 15:15:55 -0400

    Use reference in log_error

--- common/src/error.rs
+++ common/src/error.rs
@@ -3,11 +3,11 @@ use std::{backtrace::Backtrace, fmt::Debug};
 use tracing::error;
 
 pub trait LogErrorExt {
-    fn log_error(self, message: &str);
+    fn log_error(&self, message: &str);
 }
 
 impl<T, E: Debug> LogErrorExt for Result<T, E> {
-    fn log_error(self, message: &str) {
+    fn log_error(&self, message: &str) {
         if let Err(err) = self {
             error!(
                 "{}: {:?}, Backtrace: {}",
